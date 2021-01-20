1f0ffe7cb6a53c4320e53387dfc4a60739ae6f33
oleksandrLieskov <oleksandr.lsskov.98@gmail.com>
2021-01-21 00:55:55 +0200

    I2C Bridge

--- main.c
+++ main.c
@@ -13,13 +13,24 @@
 
 int main(void)
 {
+    uint8 compare = 0;
+    uint8 buf[1];
+    buf[0] = 0;
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    
+    I2C_Start();
+    UART_Start();
+    I2C_SetBuffer1(1, 1, buf);
+    
     for(;;)
     {
-        /* Place your application code here. */
+        if(compare != buf[0])
+        {
+            compare = buf[0];
+            UART_PutChar(compare);
+        }
     }
 }
 
